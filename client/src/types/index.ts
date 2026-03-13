4a50ffcdb0a48f6cc7c73b04d487a327dd6959d3
Tanmaybuilds14 <sarvetanmay276@gmail.com>
2026-03-13 11:09:07 +0530

    added the community page

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -6,3 +6,27 @@ export interface UploadZoneprops{
   onClear:()=>void;
   onChange:(e:React.ChangeEvent<HTMLInputElement>)=>void;
 }
+
+export interface User{
+  id?:string;
+  name?:string;
+  email?:string;
+}
+
+export interface Project{
+  id:string;
+  userId?:string;
+  productName:string;
+  productDescription?:string;
+  userPrompt?:string;
+  aspectRatio:string;
+  targetLenght?:string;
+  generatedImage?:string;
+  generatedVideo?:string;
+  isGenerating:boolean;
+  isPublished:boolean;
+  error?:string;
+  createdAt:Date|string;
+  updatedAt:Date|string;
+  uploadedImages:string[];
+}
