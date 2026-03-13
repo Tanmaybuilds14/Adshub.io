c9de51b934234850db3dab9767130a1edf36202b
Tanmaybuilds14 <sarvetanmay276@gmail.com>
2026-03-13 18:35:26 +0530

    added MyGenerations page

--- client/src/components/ProjectCard.tsx
+++ client/src/components/ProjectCard.tsx
@@ -2,11 +2,25 @@ import type React from "react"
 import type { Project } from "../types"
 import { useNavigate } from "react-router-dom"
 import { useState } from "react";
-import { Loader2Icon } from "lucide-react";
+import { EllipsisIcon, ImageIcon, Loader2Icon, PlaySquareIcon, Share2Icon, Trash2Icon } from "lucide-react";
+import { GhostButton, PrimaryButton } from "./Buttons";
 
 const ProjectCard = ({gen , setGeneration , forCommunity = false}:{gen:Project , setGenerations:React.Dispatch<React.SetStateAction<Project[]>>, forCommunity?:boolean}) => {
+
+
   const navigate = useNavigate();
   const [menuOpen , setMenuOpen] = useState(false);
+
+  const handledelete = async (id:string)=>{
+    const confirm = window.confirm('Are you sure to delete this project?');
+    if(!confirm)return;
+    console.log(id);
+  }
+
+  const Togglepublish = async (projectid:string)=>{
+    console.log(projectid)
+  }
+
   return (
     <div key={gen.id} className="mb-4 break-inside-avoid">
       <div className="bg-white/5 border border-white/10 rounded-xl overflow-hidden hover:border-white/20 transition group">
@@ -34,6 +48,33 @@ const ProjectCard = ({gen , setGeneration , forCommunity = false}:{gen:Project ,
               <span className="text-xs px-2 py-1 bg-green-600/30 rounded-full">Published</span>
             )}
           </div>
+          {/* action menu for my generations only */}
+          {!forCommunity && (
+            <div 
+            onMouseDownCapture={()=>setMenuOpen(true)}
+            onMouseLeave={()=>setMenuOpen(false)}
+            className="absolute right-3 top-3 sm:opacity-0 group-hover:opacity-100 transition flex items-center gap+2">
+              <div className="absolute top-3 right-3">
+                <EllipsisIcon className="ml-auto bg-black/10 rounded-full p-1 size-7"></EllipsisIcon>
+              </div>
+              <div className="flex flex-col items-end w-32 text-sm">
+                <ul className={`text-xs ${menuOpen?'block':'hidden'} overflow-hidden right-0 peer-focus:block hover:block w-40 bg-black/50 backdrop-blur text-white border border-gray-500/50 rounded-lg shadow-md mt-2 py-1 z-10`}>
+                  {gen.generatedImage && <a href="#" download className="flex gap-2 items-center px-4 py-2 hover:bg-black/10 cursor-pointer"><ImageIcon size={14}/>Download Image</a>}
+
+                  {gen.generatedVideo && <a href="#" download className="flex gap-2 items-center px-4 py-2 hover:bg-black/10 cursor-pointer"><PlaySquareIcon size={14}/>Download video</a>}
+
+                  {(gen.generatedVideo || gen.generatedImage) && <button onClick={()=>navigator.share({url:gen.generatedVideo||gen.generatedImage , title:gen.productName , text:gen.productDescription
+                  })} className="w-full flex gap-2 items-center px-4 py-2 hover:bg-black/10 cursor-pointer">
+                    <Share2Icon/>Share
+                  </button>}
+                  <button onClick={()=>handledelete(gen.id)} className="w-full flex gap-2 items-center px-4 py-2 hover:bg-red-950/10 text-red-400 cursor-pointer">
+                    <Trash2Icon size={20}/>
+                    Delete
+                  </button>
+                </ul>
+              </div>
+            </div>
+          )}
           {/* source images */}
           <div className="absolute right-3 bottom-3">
             <img src={gen.uploadedImages[0]} alt="product" className="w-16 h-16 object-cover rounded-full animate-float" />
@@ -68,7 +109,18 @@ const ProjectCard = ({gen , setGeneration , forCommunity = false}:{gen:Project ,
         {/* user prompt */}
         {gen.userPrompt && (
           <div className="mt-3">
-            <div className="text-xs text-gray-300">{gen.userPrompt}</div>
+            <p className="text-xs text-gray-400 mb-1">Prompt</p>
+            <div className="text-sm text-gray-300 bg-white/3 p-2 rounded-md wrap-break-word">{gen.userPrompt}</div>
+          </div>
+        )}
+        {!forCommunity && (
+          <div className="mt-4 grid grid-cols-2 gap-3">
+            <GhostButton className="text-xs justify-center" onClick={()=>{navigate(`/result/${gen.id}`); scrollTo(0,0)}}>
+              View Details
+            </GhostButton>
+            <PrimaryButton className="rounde-md" onClick={()=>Togglepublish(gen.id)} >
+              {gen.isPublished?'Unpublish':'Publish'}
+            </PrimaryButton>
           </div>
         )}
       </div>
