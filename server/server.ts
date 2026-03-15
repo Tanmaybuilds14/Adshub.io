d49af53bdacd8a450a4174fa6f3c6acc6effd5a5
Tanmaybuilds14 <sarvetanmay276@gmail.com>
2026-03-15 12:44:27 +0530

    DB setup

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,13 @@
 import express from 'express'
+import cors from 'cors';
+import 'dotenv/config';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
+
+//Middleware
+app.use(cors());
+app.use(express.json());
 
 app.get('/',(req,res)=>{
   res.send('server is live');
@@ -9,4 +15,4 @@ app.get('/',(req,res)=>{
 
 app.listen(PORT,()=>{
   console.log(`server is running at http://localhost:${PORT}`)
-})
+});
